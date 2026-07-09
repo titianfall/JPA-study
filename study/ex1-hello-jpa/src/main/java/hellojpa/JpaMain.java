a047ef9a0689e292cec56b56df07524be1873426
titanfall <qkrwogh7366@gmail.com>
2026-07-09 17:50:07 +0900

    feat: 영속성 전이 (단일 소유자(부모), 라이프사이클 동일)

--- study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,34 +17,32 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Team team1 = new Team();
-            team1.setName("teamA");
-            em.persist(team1);
+            Parent parent = new Parent();
+            parent.setName("Parent");
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            Child child1 = new Child();
+            child1.setName("Child1");
 
-            // 프록시 인스턴스 초기화 여부 확인
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team1);
-            em.persist(member1);
+            Child child2 = new Child();
+            child2.setName("Child2");
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            // persist를 3번이나 하게된다. 이걸 줄일수 있는 방법이 영속성 전이: cascade를 사용하는 방법이다.
+            em.persist(parent);
+            // cascade = CascadeType.ALL로 인한 생략
+            // em.persist(child1);
+            // em.persist(child2);
 
             em.flush();
             em.clear();
 
-            // FetchType.EAGER인 Member에 대해 jpql 을 사용한다 치자
-            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
-            // 실제 실행을 시켜보면 쿼리가 join 된 쿼리가 아닌 2개의 쿼리가 나가는걸 확인할 수 있다.
-            // 이걸 결과적으로 jpql N + 1 문제라고 한다. (추후 학습)
+            Parent findParent = em.find(Parent.class, parent.getId());
+            for(Child child : findParent.getChildList()){
+                System.out.println(child.getName());
+            }
 
-            // 때문에 모든 연관관계를 지연로딩으로 설정한다. jpql fetchJoin
             tx.commit();
         } catch(Exception e) {
             e.printStackTrace();

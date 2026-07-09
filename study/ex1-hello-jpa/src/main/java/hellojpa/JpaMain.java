6c4e7695100866f0a7b7d8d0c3c3432d44b37975
titanfall <qkrwogh7366@gmail.com>
2026-07-09 16:07:02 +0900

    feat: jpql을 활용한 N + 1 문제 LAZY를 사용해야 하는 이유에 대한 기초지식

--- study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,8 @@ package hellojpa;
 import jakarta.persistence.*;
 import org.hibernate.Hibernate;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -19,21 +21,30 @@ public class JpaMain {
             team1.setName("teamA");
             em.persist(team1);
 
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
             // 프록시 인스턴스 초기화 여부 확인
             Member member1 = new Member();
             member1.setUsername("member1");
             member1.setTeam(team1);
             em.persist(member1);
 
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team2);
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-            System.out.println(m.getTeam().getClass()); // FetchType.LAZY : 프록시 초기화 확인 가능
-            System.out.println("========");
-            m.getTeam().getName(); // 프록시 객체 초기화 및 db 쿼리
-            System.out.println("=========");
+            // FetchType.EAGER인 Member에 대해 jpql 을 사용한다 치자
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
+            // 실제 실행을 시켜보면 쿼리가 join 된 쿼리가 아닌 2개의 쿼리가 나가는걸 확인할 수 있다.
+            // 이걸 결과적으로 jpql N + 1 문제라고 한다. (추후 학습)
 
+            // 때문에 모든 연관관계를 지연로딩으로 설정한다. jpql fetchJoin
             tx.commit();
         } catch(Exception e) {
             e.printStackTrace();

e002a5679dad41f4f952275966a5bc5b014becee
titanfall <qkrwogh7366@gmail.com>
2026-07-09 20:33:41 +0900

    feat: 불변 객체의 값을 수정하는법

--- study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,31 +20,17 @@ public class JpaMain {
         try{
             Address address = new Address("city", "street", "10000");
 
-            // 가정 : 동일한 주소를 쓰는 member1 과 member2가 있다
             Member member1 = new Member();
             member1.setUsername("member1");
             member1.setWorkPeriod(new Period(LocalDateTime.now()));
             member1.setHomeAddress(address);
 
-            // 값을 복사하여 사용하면 참조 에러를 막을 수 있다.
-            // copyAddress = address 처럼 자바는 공유 참조를 막을수는 없다. 컴파일러 레벨에서 해결이 불가능
-            // 때문에 객체 타입을 수정할 수 없게 만들면 부작용을 원천 차단할 수 있다.
-            // 생성자로만 값을 설정, 수정자(setter) 만들지 않음. or Integer 나 String을 사용하면 된다.
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setWorkPeriod(new Period(LocalDateTime.now()));
-            // member2.setHomeAddress(address) or member.getHomeAddress()
-            member2.setHomeAddress(copyAddress);
+            // 값 타입을 불변 객체로 선언하였는데, 실제로 값을 변경할 일이 생겼다고 가정하자.
+            // 여러 방법이 있지만 새로운 객체를 만들어 reference를 갈아 끼우는것도 하나의 방법이다.
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member1.setHomeAddress(newAddress);
 
             em.persist(member1);
-            em.persist(member2);
-
-            // 불변 값을 통해 선언을 하였다.
-            member1.getHomeAddress().setCity("newCity");
-
-            em.flush();
-            em.clear();
 
             tx.commit();
         } catch(Exception e) {

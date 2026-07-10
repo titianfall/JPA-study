b518d1e1dbfabfc3771b6a68d5aaae5408062574
titanfall <qkrwogh7366@gmail.com>
2026-07-10 15:21:58 +0900

    feat: 프로젝션(select)

--- study/hello-jpql/src/main/java/hellojpql/JpaMain.java
+++ study/hello-jpql/src/main/java/hellojpql/JpaMain.java
@@ -3,6 +3,7 @@ package hellojpql;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,27 +18,26 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            // 반환 타입이 분명할 경우 TypedQuery<T> 사용
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m where m.id = 10L", Member.class);
-            // TypedQuery<String> query2 = em.createQuery("select m.username from Member m where m.username = :username", String.class);
-            TypedQuery<Member> query2 = em.createQuery("select m from Member m where m.username = ?1", Member.class);
-            Member singleResult = query2.setParameter(1, member.getUsername()).getSingleResult();
-            // String singleResult = query2.setParameter("username", "member1").getSingleResult();
-            System.out.println(singleResult);
+            em.flush();
+            em.clear();
 
-            // 반환 타입이 불분명할 경우에는 Query 사용
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            System.out.println("========================START============================");
+            // 프로젝션 대상 : 엔티티 / 임베디드 타입 / 스칼라 타입
+            // 엔티티 프로젝션
+            em.createQuery("select m from Member m", Member.class);
+            // 묵시적 조인
+            List<Team> result = em.createQuery("select m.team from Member m", Team.class).getResultList();
 
-            // 반환 타입이 한개일 경우 (값이 있을때 써야함)
-            // 결과가 없을 경우   : jakarta.persistence.NoResultException
-            // 결과가 여러개일 경우: jakarta.persistence.NonUniqueResultException
-            // spring data jpa > getSingleResult() > try-catch > null or Optinal 반환
-            System.out.println("======================getSingleResult()==================");
-            Member result = em.createQuery("select m from Member m", Member.class).getSingleResult();
+            // 임베디드 타입 프로젝션
+            List<Address> resultAddress = em.createQuery("select o.address from Order o", Address.class).getResultList();
 
-            // 반환 타입이 컬렉션일경우 - 값이 없으면 빈 컬렉션 반환(NPE 걱정은 안해도 됨)
-            System.out.println("=======================getResultList()=======================");
-            List<Member> resultList = em.createQuery("select m from Member m", Member.class).getResultList();
+            // 스칼라 타입 프로젝션
+            List resultList = em.createQuery("select m.username, m.age from Member m").getResultList();
+            Object o = resultList.get(0);
+            Object[] resultObject = (Object[]) o;
+
+            System.out.println(resultObject[0]);
+            System.out.println(resultObject[1]);
 
             tx.commit();
         } catch(Exception e){

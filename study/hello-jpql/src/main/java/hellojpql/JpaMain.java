9c82eb0f92f33db54ab201ee6eb0d64f40e1d602
titanfall <qkrwogh7366@gmail.com>
2026-07-10 17:47:39 +0900

    feat: 조건식 case when, colesce, NULLIF

--- study/hello-jpql/src/main/java/hellojpql/JpaMain.java
+++ study/hello-jpql/src/main/java/hellojpql/JpaMain.java
@@ -29,19 +29,31 @@ public class JpaMain {
             em.clear();
 
             System.out.println("========================START============================");
-            String jpql = "select m.username, 'HELLO', true from Member m " +
-                    // "where m.type = hellojpql.MemberType.ADMIN"; // 파라미터 바인딩으로 줄일수 있음
-                     "where m.type = :userType";
-            List resultList1 = em.createQuery(jpql)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
-            Object o = (Object []) resultList1.get(0);
-            Object[] result =  (Object[]) o;
-            for(Object o1 : result){
-                System.out.println(o1);
+            String jpql = "select " +
+                            "case when m.age <= 10 then '학생요금' " +
+                            "      when m.age >=  60 then '경로요금' " +
+                            "else '일반요금' " +
+                            "end " +
+                            "from Member m";
+
+            List<String> resultList = em.createQuery(jpql, String.class).getResultList();
+            for(String s : resultList){
+                System.out.println(s);
             }
 
+            // 하나씩 조회하여 null이 아니면 반환
+            jpql = "select coalesce(m.username, '이름없는 회원') from Member m";
+            List<String> resultList1 = em.createQuery(jpql, String.class).getResultList();
+            for (String str : resultList1) {
+                System.out.println(str);
+            }
 
+            // NULLIF 두 값이 같으면 null 반환, 다르면 첫번째 값 반환
+            jpql = "select NULLIF(m.username, '관리자') from Member m";
+            List<String> resultList2 = em.createQuery(jpql, String.class).getResultList();
+            for (String str : resultList2) {
+                System.out.println(str);
+            }
             tx.commit();
         } catch(Exception e){
             e.printStackTrace();

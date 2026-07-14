b3eb1f8dc0575e72c4199380ad2c2bf9d7dd747b
titanfall <qkrwogh7366@gmail.com>
2026-07-14 13:02:00 +0900

    feat: Service 비즈니스 로직 작성 및 Injection 방법 @RequiredArgsConstructor

--- study/SpringBootJPA/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ study/SpringBootJPA/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,19 +3,21 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository // @Component 내장 컴포넌트 스캔대상
+@RequiredArgsConstructor
 public class MemberRepository {
 
     // @PersistenceUnit
     // private EntityManagerFactory emf; // 쓸일은 없음
 
     // 자동으로 emf 로부터 em를 주입받음
-    @PersistenceContext
-    private EntityManager em;
+    // @PersistenceContext
+    private final EntityManager em;
 
     // cmd + shift + T > test 파일 생성
     public void save(Member member) {
